f733220fd81eca92fa7d8f428d8865151317dfaa
Bill Mårtensson <bill@magictechnology.se>
2022-10-13 09:08:24 +0200

    Initial Commit

---
+++ pia11v5listnav/pia11v5listnavApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia11v5listnavApp.swift
+//  pia11v5listnav
+//
+//  Created by Bill Martensson on 2022-10-13.
+//
+
+import SwiftUI
+
+@main
+struct pia11v5listnavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
